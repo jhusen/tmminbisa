f1a33a920e73fc85fcb860cc1236e8117754080a
jhusen <jhusen@users.noreply.github.com>
2020-10-25 20:24:38 +0700

    Convert HTML ke svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: 'First Charity Project',
+      category: 'Money',
+      thumbnail: 'https://images.unsplash.com/photo-1518398046578-8cca57782e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      pledged: 50000,
+      target: 15000000,
+      date_end: +new Date('10 November 2020'),
+      profile_photo:'https://instagram.fcgk4-3.fna.fbcdn.net/v/t51.2885-19/s150x150/119475376_447116476262846_1507504007353770627_n.jpg?_nc_ht=instagram.fcgk4-3.fna.fbcdn.net&_nc_cat=102&_nc_ohc=W_o3nuOfQMQAX8dO53a&oh=4fee759ff59b55bbccf2998924a161db&oe=5FBD77D1',
+      profile_name: 'Jahid Husen',
+      no_pledges: 0
+    }
+  ]
+}
